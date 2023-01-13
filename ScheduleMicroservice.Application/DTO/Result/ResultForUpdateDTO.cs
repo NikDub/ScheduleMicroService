a1b6533faca8d24ebe9935eb6b466898deebeaa0
NikDub <37387164+NikDub@users.noreply.github.com>
2023-01-13 16:53:54 +0300

    Delete ScheduleMicroservice.Application/DTO directory

--- ScheduleMicroservice.Application/DTO/Result/ResultForUpdateDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ScheduleMicroservice.Application.DTO.Result;
-
-public class ResultForUpdateDto
-{
-    public Guid AppointmentsId { get; set; }
-    public string Complaints { get; set; }
-    public string Conclusion { get; set; }
-    public string Recommendations { get; set; }
-}
